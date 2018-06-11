29a70fb2fb87cbedeac2160daf6b0ba8d5d517f7
riteshby24 <40039704+riteshby24@users.noreply.github.com>
2018-06-12 00:56:35 +0530

    Update SampleGit.java

--- Githubsample/src/com/githubsample/SampleGit.java
+++ Githubsample/src/com/githubsample/SampleGit.java
@@ -1,6 +1,11 @@
 package com.githubsample;
 
-public class SampleGit {
+public class SampleGit 
+{
+	public void run()
+	{
+	  System.out.println("in run");	
+	}
 
 	public static void main(String[] args) {
 		System.out.println("I Love java");
