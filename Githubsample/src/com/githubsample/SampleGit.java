e8fd4671f791e4c18557a4274b17eeff98ab8b3a
riteshby24 <40039704+riteshby24@users.noreply.github.com>
2018-06-12 00:50:25 +0530

    Update SampleGit.java

--- Githubsample/src/com/githubsample/SampleGit.java
+++ Githubsample/src/com/githubsample/SampleGit.java
@@ -6,6 +6,7 @@ public class SampleGit {
 		System.out.println("I Love java");
 		System.out.println("I Love Selenium");
 		System.out.println("I Love Selenium");
+		System.out.println("I Love ManualTesting");
 	}
 
 }
