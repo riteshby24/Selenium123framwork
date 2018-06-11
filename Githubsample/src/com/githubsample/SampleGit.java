7536061aa60020991adbd78c4f67bd80a64bbee5
Ritesh <ritcool.soni@gmail.com>
2018-06-11 17:10:55 +0530

    first git smaple

---
+++ Githubsample/src/com/githubsample/SampleGit.java
@@ -0,0 +1,11 @@
+package com.githubsample;
+
+public class SampleGit {
+
+	public static void main(String[] args) {
+		System.out.println("I Love java");
+		System.out.println("I Love Selenium");
+
+	}
+
+}
