503aaebe5d97f26e8a7557cf800154cb9315a0e5
Ritesh <ritcool.soni@gmail.com>
2018-06-11 18:03:31 +0530

    sdfsfdsf

--- Githubsample/src/com/githubsample/SampleGit.java
+++ Githubsample/src/com/githubsample/SampleGit.java
@@ -5,7 +5,7 @@ public class SampleGit {
 	public static void main(String[] args) {
 		System.out.println("I Love java");
 		System.out.println("I Love Selenium");
-
+		System.out.println("I Love Selenium");
 	}
 
 }
